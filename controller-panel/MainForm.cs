20253fe700273805bf4fcc33eec262f17672ebfd
Tom Gregor <tgregor@ivsoftware.com>
2023-04-22 15:49:31 -0600

    Tested media player using WPF.

--- controller-panel/MainForm.cs
+++ controller-panel/MainForm.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace controller_panel
 {
@@ -28,6 +30,21 @@ namespace controller_panel
                     control.Width = flowLayoutPanel.Width - SystemInformation.VerticalScrollBarWidth;
                 }
             };
+        }
+        private MediaPlayer MediaPlayer { get; } = new MediaPlayer();
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            MediaPlayer.MediaOpened += (sender, e) =>
+            {
+                MediaPlayer.Play();
+            };
+            var path = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "MP3",
+                "sample-3s.mp3"
+                );
+            MediaPlayer.Open(new Uri(path));
         }
     }
 }
